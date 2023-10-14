fca8ee22f096759a2b918799fdc886146c843162
Cristian <cristian-02-@live.com>
2023-10-13 23:32:59 -0500

    Creando los token de authenticación

--- routes/token.py
+++ routes/token.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
 from typing import Annotated
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
+from config.database import db_dependency
+from models.usuario import Usuarios
+from passlib.context import CryptContext
 
 
 router = APIRouter(
@@ -9,6 +12,27 @@ router = APIRouter(
 )
 
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+form_data_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+def authenticate_user(usuario: str, contrasena: str, db):
+    usuario = db.query(Usuarios).filter(Usuarios.usuario == usuario).first()
+
+    if not usuario:
+        return False
+    if not bcrypt_context.verify(contrasena, usuario.contrasena):
+        return False
+    
+    return usuario
+
+
 @router.post('/token', status_code=status.HTTP_201_CREATED)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    return 'token'
+async def login_for_access_token(form_data: form_data_dependency, db: db_dependency):
+    
+    usuario = authenticate_user(form_data.username, form_data.password, db)
+
+    if not usuario:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZE, detail='Usuario no valido')
+    
+    # token = 
